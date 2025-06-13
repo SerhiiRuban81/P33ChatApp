4c3d06958da2b821c9ffdda1b96183994e280d2f
serhii.ruban81 <serhii.ruban81@gmail.com>
2025-06-13 18:56:09 +0300

    EF and some basic classes added

---
+++ P33ChatApp/Data/ChatContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P33ChatApp.Data
+{
+    public class ChatContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ChatContext(DbContextOptions<ChatContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=P33ChatDb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ P33ChatApp/Data/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P33ChatApp.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = default!;
+
+        public string Password { get; set; } = default!;
+
+        public string Email  { get; set; } = default!;
+
+        public string IpAddress  { get; set; } = default!;
+
+        public int Port { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
