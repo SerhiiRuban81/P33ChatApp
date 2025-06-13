58fbf7ea906528472934e455c70e49d9f4f9c03a
gdenis 1 <vitalij.poleshhuk.04@gmail.com>
2025-06-13 19:17:04 +0300

    chats added context

--- P33ChatApp/Data/ChatContext.cs
+++ P33ChatApp/Data/ChatContext.cs
@@ -10,6 +10,8 @@ namespace P33ChatApp.Data
     public class ChatContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+
 
         public ChatContext(DbContextOptions<ChatContext> options) : base(options) { }
 
